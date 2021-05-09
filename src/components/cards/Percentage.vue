5eb495564e9351e54fd334ffd3a2350f73885e1c
khanr <khanriza@gmail.com>
2021-05-09 10:08:02 -0400

    added a card type

---
+++ src/components/cards/Percentage.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="percentage">
+    <p class="percentage__title">{{ title }}</p>
+    <h1 class="percentage__percent">{{ percent }}%</h1>
+    <p class="percentage__description">{{ description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+
+    percent: {
+      type: Number,
+      default: 0,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.percentage {
+  padding: 0.5rem;
+  display: flex;
+  border-radius: 5px;
+  flex-direction: column;
+  text-align: center;
+  transition: all 0.5s ease;
+  width: 275px;
+
+  &__title {
+    margin: auto;
+    padding-bottom: 0.75rem;
+    border-bottom: 1px solid grey;
+    text-transform: uppercase;
+  }
+  &__percent {
+    color: #1ac6b8;
+    font-size: 64px;
+  }
+  &__description {
+    width: 80%;
+    margin: auto;
+  }
+
+  &:hover {
+    box-shadow: 1px 1px 17px 0 rgba(0, 0, 1, 0.72);
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,6 +9,26 @@ relinquishing ownership and burnt LP from the beginning of its creation."
       iframeUrl="https://www.youtube.com/embed/jqidVJnbkN0?rel=0&controls=1&autoplay=0&mute=0&start=0"
     />
 
+    <div class="landing__cards percentages">
+      <Percentage
+        title="Reflection"
+        :percent="2"
+        description="Fee redistributed to all holders including black hole"
+      />
+
+      <Percentage
+        title="Liquidity Accumulation"
+        :percent="2"
+        description="Fee on sell trades, added to the liquidity pool by smart contract locked forever"
+      />
+
+      <Percentage
+        title="Burning"
+        :percent="50"
+        description="Burnt to the black hole, getting bigger from the fees."
+      />
+    </div>
+
     <Jumbotron
       :actions="secondaryJumboActions"
       subtitle="Security"
@@ -21,10 +41,11 @@ relinquishing ownership and burnt LP from the beginning of its creation."
 
 <script>
 import Jumbotron from "@/components/compound/Jumbotron";
+import Percentage from "@/components/cards/Percentage";
 
 export default {
   name: "Home",
-  components: { Jumbotron },
+  components: { Jumbotron, Percentage },
   computed: {
     mainJumboActions() {
       const actions = [];
@@ -41,4 +62,11 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.landing {
+  &__cards {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
+</style>
